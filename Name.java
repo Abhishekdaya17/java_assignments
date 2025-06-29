5f47c7b21f5deb14d01d6cd7415aee66b2ba66ad
avinash-9893 <vermaavinash692@gmail.com>
2025-06-30 00:47:39 +0530

    Uploaded all Java assignments

--- Name.java
+++ Name.java
@@ -1,7 +1,7 @@
 class Base{
     static void info(){
-        System.out.println("Name : Abhishek Daya");
-        System.out.println("Enrolment Number : 0873CS231002 \n");
+        System.out.println("Name : Avinash Verma");
+        System.out.println("Enrolment Number : 0873CS231022 \n");
     }
 }
 public class Name extends Base{
